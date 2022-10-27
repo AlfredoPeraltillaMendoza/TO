906de155f17d7e6917ba83ed3f9374fb866e06cf
Alfredo Peraltilla <aperaltilla@unsa.edu.pe>
2022-10-27 14:12:17 -0500

    Assert try in impl
    Verify conditions and no adding charge in prog

--- StackArray.cc
+++ StackArray.cc
@@ -9,23 +9,27 @@ void StackArray<T>::push(T e)
     resize();
   *sp = e;
   sp++;
-  
+ // assert(e==*sp); 
 }
 
 template <typename T>
-void StackArray<T>::pop()
-{ }
+void StackArray<T>::pop(){ 
+  // assert(!empty()); 
+  sp--; 
+  }
 
 template <typename T>
 T StackArray<T>::top()
 {
   return *(sp - 1);
+  //assert(*(sp-1)==T[size])) 
 }
 
 template <typename T>
 bool StackArray<T>::empty()
 {
   return sp == data;
+  //assert(sp==data);
 }
 
 template <typename T>
