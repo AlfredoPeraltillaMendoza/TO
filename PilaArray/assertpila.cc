5fdd1a40be3e36d29fc8d0b5c076f464cfd855c4
Alfredo Alejandro Peraltilla Mendoza <aperaltilla@unsa.edu.pe>
2022-11-03 04:47:02 -0500

    Intento de ejecuccion

--- PilaArray/assertpila.cc
+++ PilaArray/assertpila.cc
@@ -1,9 +1,18 @@
 #include "StackArray.cc"
-#include <iostream>
+
 // argument count and argument vector
 //int main(int argc, char *argv[])
-int main(int num_args, char** arg_strings)
+//int num_args, char** arg_strings
+int main()
 {
-    
+StackArray<int>x(3);
+  
+  x.push(1);
+  x.push(2);
+  x.push(3);
+  x.push(4);
+  x.empty();
+
+  return 0;
     return 0;
 }
